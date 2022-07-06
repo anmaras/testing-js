f93cfd9815426c2c96ef853a01bead0ee8f9ba16
Antonis <marasantonis@gmail.com>
2022-07-06 17:10:25 +0300

    object methods testing

--- functions.js
+++ functions.js
@@ -5,3 +5,24 @@ export function sum(a, b) {
 export function capitalize(str) {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
+
+export function reverseStr(str) {
+  return str.split('').reverse().join('');
+}
+
+export const calculator = {
+  add(a, b) {
+    return Number(a + b);
+  },
+  subtract(a, b) {
+    return a - b;
+  },
+
+  divide(a, b) {
+    return a / b;
+  },
+
+  multiply(a, b) {
+    return a * b;
+  },
+};
--- functions.test.js
+++ functions.test.js
@@ -1,4 +1,4 @@
-import { sum, capitalize } from './functions';
+import { sum, capitalize, reverseStr, calculator } from './functions';
 
 test('two plus two is four', () => {
   expect(sum(1, 1)).toBe(2);
@@ -6,4 +6,21 @@ test('two plus two is four', () => {
 
 test('first letter is uppercase', () => {
   expect(capitalize('john')).toBe('John');
+});
+
+test('string return reversed', () => {
+  expect(reverseStr('nikos')).toBe('sokin');
+});
+
+test('one plus one is two', () => {
+  expect(calculator.add(1, 1)).toBe(2);
+});
+test('one minus one is zero', () => {
+  expect(calculator.subtract(1, 1)).toBe(0);
+});
+test('one divided by one is 1', () => {
+  expect(calculator.divide(1, 1)).toBe(1);
+});
+test('tho multiply by six is twelve', () => {
+  expect(calculator.multiply(2, 6)).toBe(12);
 });
