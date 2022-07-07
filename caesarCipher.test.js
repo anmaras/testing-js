e70ec92bb21e9918a06c538cf7cf262b52ec8b35
Antonis <marasantonis@gmail.com>
2022-07-07 11:31:31 +0300

    casesarChipher  tests update

--- caesarCipher.test.js
+++ caesarCipher.test.js
@@ -1,5 +1,21 @@
 import caesarCipher from './caesarCipher';
 
+test('when char is z then it turns a', () => {
+  expect(caesarCipher('z')).toBe('a');
+});
+
+test('when char is number then its number plus 1', () => {
+  expect(caesarCipher(1)).toBe('2');
+});
+
+test('when char is uppercase or lowercase the case wont change', () => {
+  expect(caesarCipher('A')).toBe('B');
+});
+
+test('special characters wont change', () => {
+  expect(caesarCipher('@')).toBe('@');
+});
+
 test('a returns b', () => {
   expect(caesarCipher('a')).toBe('b');
 });
